0c432417967c649d0aae78262e274194e7e093ae
Joe Speakman <joseph.speakman@me.com>
2022-01-16 18:30:54 -0500

    Adding details to main timer page

--- Timeless/TimeBoard.swift
+++ Timeless/TimeBoard.swift
@@ -8,24 +8,22 @@
 import SwiftUI
 
 struct TimeBoard: View {
+    let Const = Constants.TimeBoard.self
+    
     @State private var selectedTab = 1
-    @State private var countOfTimer = [0]
+    @State private var countOfTimers = [0]
     @ObservedObject var timer: FirstTimer
 
     var body: some View {
         NavigationView{
-            
-            List {
-                ForEach(countOfTimer, id: \.self ) { countOfTimer in
-                    if $countOfTimer.contains(where: 0) {
-                        EmptyView()
-                        //TODO: Add Get Started View.
-                    }
+            ScrollView{
+//            List {
+                ForEach(countOfTimers, id: \.self ) { countOfTimers in
                         GroupBox() {
                             HStack {
                                 Text(timer.emoji)
                                     .font(.largeTitle)
-        //                            .multilineTextAlignment(.center)
+//                                    .multilineTextAlignment(.center)
                                 Spacer()
                                 // Example Text is for Label for Object name
                                 Text(timer.name)
@@ -53,49 +51,26 @@ struct TimeBoard: View {
                                 
                         }
                         .padding()
-                    } else {
-                        /*@START_MENU_TOKEN@*/EmptyView()/*@END_MENU_TOKEN@*/
                     }
                     
                 
-                }
-
-//                GroupBox() {
-//                    HStack {
-//                        Text(timer.emoji)
-//                            .font(.headline)
-//                        Spacer()
-//                        // Example Text is for Label for Object name
-//                        Text(timer.name)
-//                            .font(.headline.bold())
-//                        Spacer()
-//                        //Button needs function added
-//                        Button(action: {
-//                            print("Put function info here")
-//                        }) {
-//                            // Button Color Needs Changing.
-//                            Image(systemName: "square.and.arrow.up")
-//                                .font(Font.system(.largeTitle).bold())
-//
-//                        }
-//                    }
-//
-//                        VStack {
-//                            Text("Time Left")
-//                                .font(.headline)
-//                            Text("140 Days")
-//                                .font(.largeTitle).bold()
-//                            Text("1 Hour, 48 Minutes")
-//                        }
-//
-//
 //                }
-//                .padding()
             }
             .padding(.top, 10.0)
-        .navigationTitle("Timeless")
+            .navigationTitle(Constants.Labels.appName)
+        .toolbar {
+            Button {
+                //TODO: Create Modal to add in countdown.
+                countOfTimers.append(countOfTimers.count+1)
+            } label: {
+                Constants.Images.plus
+            }
+
         }
-    }
+            }
+
+        }
+    
 }
 
 struct TimeBoard_Previews: PreviewProvider {
---
+++ Timeless/Utils/Constants.swift
@@ -0,0 +1,27 @@
+//
+//  Constants.swift
+//  Timeless
+//
+//  Created by Joe Speakman on 1/16/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct Constants {
+    
+    struct Images {
+        static var plus = Image(systemName: "plus")
+        static var share = Image(systemName: "square.and.arrow.up")
+    }
+    struct Labels {
+        static var appName = "Cats Does Countdown"
+    }
+    
+    struct SampleData {
+//        static var
+    }
+    struct TimeBoard {
+        static var padding = 10.0
+    }
+}
